d6507db1c970fd24083ce4b1b56abff77de98059
Tiedaman33 <orinabravin6@gmail.com>
2024-03-27 20:11:41 -0700

    bin

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,9 +26,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t l = 0, r = 0;
 
-		l = tree->left > 1 + binary_tree_height(tree->left) : 1;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > r) ? 1 : r);
+		return ((l > r) ? l : r);
 	}
 	return (0);
 }
