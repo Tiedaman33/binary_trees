222cce3dec35c49c6167d9eb274b86cb8dde4993
Tiedaman33 <orinabravin6@gmail.com>
2024-03-27 20:05:53 -0700

    bin

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		nodes += (tree->left || tree->right) ? l : 0;
+		nodes += (tree->left || tree->right) ? 1 : 0;
 		nodes += binary_tree_nodes(tree->left);
 		nodes += binary_tree_nodes(tree->right);
 	}
