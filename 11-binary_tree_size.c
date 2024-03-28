ad3eb9dcae1703d5bc89fc0c43be7ec5f7620e00
Tiedaman33 <orinabravin6@gmail.com>
2024-03-27 20:04:54 -0700

    bin

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,7 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		size += l;
+		size += 1;
 		size += binary_tree_size(tree->left);
 		size += binary_tree_size(tree->right);
 	}
